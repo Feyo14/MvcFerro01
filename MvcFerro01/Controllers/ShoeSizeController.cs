158be911e75b748aff6bb40d84939ffc05793e5e
matias ferro <maty_ferro_96@hotmail.com>
2024-11-08 17:48:24 -0300

    listo

--- MvcFerro01/Controllers/ShoeSizeController.cs
+++ MvcFerro01/Controllers/ShoeSizeController.cs
@@ -202,7 +202,7 @@ namespace MvcFerro01.Controllers
             {
                 ShoeSize SVm = _mapper.Map<ShoeSize>(shoVm);
 
-                if (!service.existe(SVm))
+                if (service.existe(SVm))
                 {
                     shoVm.Shoe = serviceB!
                                  .GetAll(orderBy: q => q.OrderBy(c => c.Descripcion))
@@ -221,9 +221,16 @@ namespace MvcFerro01.Controllers
 
                     return View(shoVm);
                 }
-             
-           service.Agregar(SVm);
-           TempData["success"] = "Record successfully added/edited";
+                if (!service.existe(SVm))
+                {
+                    service.Agregar(SVm);
+                    TempData["success"] = "Record successfully added/edited";
+                }
+                else
+                {
+                    TempData["Error"] = "Record Exist";
+                }
+          
            return RedirectToAction("Index");
                 
             }
